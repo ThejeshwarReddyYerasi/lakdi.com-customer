6dc8e144ba2dd5d760e15076714bccba5d00a250
ThejeshwarReddyYerasi <thejeshwar.reddy@coviam.com>
2020-01-19 15:14:42 +0530

    vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,31 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
+    <navbar></navbar>
+    <!-- <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+      <router-link to="/theja">Theja</router-link> -->
     <router-view/>
   </div>
 </template>
+<script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+import navbar from './components/Navbar'
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  components: {
+    navbar,
+  },
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style scoped>
+#id{
+  background-color: aqua !important;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card >
+    <v-toolbar style="background:red">
+      theja
+    </v-toolbar>
+  </v-card>
+</template>
+<script>
+  export default {
+    data: () => ({
+      
+    }),
+  }
+</script>
+<style scoped>
+#id{
+  background-color: aqua;
+}
+</style>
